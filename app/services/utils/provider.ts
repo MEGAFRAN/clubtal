9ca6b5cfbf7f845fbf3bb22a9c6c8e4f8da743b6
Francisco Cruz <megafran@msn.com>
2022-02-21 11:54:12 -0500

    (build): added some useful styles layouts and reduced provider.ts code size

--- app/services/utils/provider.ts
+++ app/services/utils/provider.ts
@@ -2,61 +2,37 @@ import axios, { AxiosRequestConfig } from 'axios';
  
 const getAll = async (apiUrl:string, endpoint:string) => 
 { 
-  try 
-  {
-    let response = await axios.get(`${apiUrl}/${endpoint}`)
-    return response
-  } 
+  try {let response = await axios.get(`${apiUrl}/${endpoint}`); return response} 
   catch (error) {console.error(error)}
 }
 
 const getSingle = async (apiUrl:string, endpoint:string, id:AxiosRequestConfig<any>) => 
 { 
-  try 
-  {
-    let response = await axios.get(`${apiUrl}/${endpoint}/${id}`) 
-    return response
-  } 
+  try {let response = await axios.get(`${apiUrl}/${endpoint}/${id}`); return response} 
   catch (error) {console.error(error)}
 }
 
 const post = async (apiUrl:string, endpoint:string, model:object) => 
 { 
-  try 
-  {
-    let response = await axios.post(`${apiUrl}/${endpoint}`, model)
-    return response
-  } 
+  try {let response = await axios.post(`${apiUrl}/${endpoint}`, model); return response} 
   catch (error) {console.error(error)}
 }
 
 const put = async (apiUrl:string, endpoint:string, model:object) => 
 { 
-  try 
-  {
-    let response = await axios.put(`${apiUrl}/${endpoint}`, model)
-    return response
-  } 
+  try {let response = await axios.put(`${apiUrl}/${endpoint}`, model); return response} 
   catch (error) {console.error(error)}
 }
 
 const patch = async (apiUrl:string, endpoint:string, model:object) => 
 { 
-  try 
-  {
-    let response = await axios.patch(`${apiUrl}/${endpoint}`, model) 
-    return response
-  } 
+  try {let response = await axios.patch(`${apiUrl}/${endpoint}`, model); return response} 
   catch (error) {console.error(error)}
 }
 
 const remove = async (apiUrl:string, endpoint:string, id:any) => 
 { 
-  try 
-  {
-    let response = await axios.delete(`${apiUrl}/${endpoint}`, id)  
-    return response
-  } 
+  try {let response = await axios.delete(`${apiUrl}/${endpoint}`, id);  return response} 
   catch (error) {console.error(error)}
 }
 
