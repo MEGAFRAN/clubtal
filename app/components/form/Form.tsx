e87942c1fcbdcb4d217d4a6c767a244f1aea0a11
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2022-05-02 22:25:38 -0500

    Fix (form): Add tags to the form to improve the semantics and also add back the styles with these changes.

--- app/components/form/Form.tsx
+++ app/components/form/Form.tsx
@@ -9,27 +9,42 @@ export const Form = ({text}: any) => {
   const [formMessage, setFormMessage] = useState<string>('')
   const [formName, setFormName] = useState<string>('')
   const [formEmail, setFormEmail] = useState<string>('')
-  
+
 
   const onSubmit = (event:any): void => {
     event.preventDefault()
     sendFormMessage(formMessage, formName, formEmail, setMessageResponseStatus, setMessageResponse)
   }
-    
+
 
   return (
 
     <form className={styles.container} onSubmit={onSubmit}>
-          
-        <h2>{text[0]}</h2>
-        <label>{text[1]}</label>
-        <textarea required placeholder={text[2]} cols={30} rows={3} onChange={event => setFormMessage(event.target.value)}></textarea>
-        <label>{text[3]}</label>
-        <input type="text" required placeholder={text[4]} onChange={event => setFormName(event.target.value)}/>
-        <label>{text[5]}</label>
-        <input type="text" required placeholder={text[6]} onChange={event => setFormEmail(event.target.value)} />
-        <p className={`response-mensaje--${messageResponseStatus}`}>{messageResponse}</p>
-        <button type="submit">{text[7]}</button>
+
+      <h2>{text[0]}</h2>
+
+      <label form='message'>
+        <span>{text[1]}</span>
+        <textarea name='message' id='message' required placeholder={text[2]} cols={30} rows={3} onChange={event => setFormMessage(event.target.value)}></textarea>
+      </label>
+
+      <label form='name'>
+        <span>{text[3]}</span>
+        <input name='name' id='name' autoComplete='name' type="text" required placeholder={text[4]} onChange={event => setFormName(event.target.value)}/>
+      </label>
+
+      <label form='email'>
+        <span>{text[5]}</span>
+        <input name='email' id='email' autoComplete='email' type="email" required placeholder={text[6]} onChange={event => setFormEmail(event.target.value)} />
+      </label>
+
+      <p className={`response-mensaje--${messageResponseStatus}`}>{messageResponse}</p>
+
+      <button type="submit">{text[7]}</button>
+
+
+
+
 
     </form>
 
