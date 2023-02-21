cb613f9ec08646ca993cefa95fed8abbd776265d
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2023-02-21 10:46:05 -0500

    Feature(WEB-34): Create page for vacancy marketing digital with its information

---
+++ app/services/pages/vacancy/marketing-digital.ts
@@ -0,0 +1,30 @@
+export const Vacancy = {
+  marketingDigital: {
+    title: "Markenting Digital",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, aliquid. Exercitationem, distinctio! Blanditiis, quas vel. Expedita eius consequatur fuga facere voluptas architecto non cumque autem possimus, unde aliquid voluptatibus. Inventore!",
+    taskJob: {
+      title: "Labores del cargo",
+      description: [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, aliquid. Exercitationem, distinctio! Blanditiis, quas vel. Expedita eius consequatur fuga facere voluptas architecto non cumque autem possimus, unde aliquid voluptatibus. Inventore!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, aliquid. Exercitationem, distinctio! Blanditiis, quas vel. Expedita eius consequatur fuga facere voluptas architecto non cumque autem possimus, unde aliquid voluptatibus. Inventore!",
+      ],
+      id: "section-tasks-job",
+    },
+    requirementsJob: {
+      title: "Habilidades requeridas",
+      description: [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, aliquid. Exercitationem, distinctio! Blanditiis, quas vel. Expedita eius consequatur fuga facere voluptas architecto non cumque autem possimus, unde aliquid voluptatibus. Inventore!",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, aliquid. Exercitationem, distinctio! Blanditiis, quas vel. Expedita eius consequatur fuga facere voluptas architecto non cumque autem possimus, unde aliquid voluptatibus. Inventore!",
+      ],
+      id: "section-requirements-job",
+    },
+    aboutCompany: {
+      title: "Quines somos",
+      description: [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, aliquid. Exercitationem, distinctio! Blanditiis, quas vel. Expedita eius consequatur fuga facere voluptas architecto non cumque autem possimus, unde aliquid voluptatibus. Inventore!",
+      ],
+      id: "section-who-we-are",
+    },
+  },
+}
