7da4c3a34b9e90158c966426ab4e68841e22ea01
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2023-02-21 10:48:02 -0500

    Style(WEB-34): Add styles a page with mobile first

--- app/components/templates/vacancy/JobDetail.tsx
+++ app/components/templates/vacancy/JobDetail.tsx
@@ -3,7 +3,7 @@ import { Form } from "../../form/Form"
 import { Title } from "../../heading/Heading"
 import { Navbar } from "../../sections/above_fold/navbar/Navbar"
 import { SectionTitleList } from "../../sections/title_list/SectionTitleList"
-
+import styles from "../../../styles/layouts/vacancyCurrent.module.scss"
 export const JobDetail = ({
   options,
   buttonText,
@@ -17,14 +17,14 @@ export const JobDetail = ({
   text,
 }: JobDetailProps) => {
   return (
-    <main>
+    <main className={styles.container}>
       <Navbar
         options={options}
         buttonText={buttonText}
         mail={mail}
         withLanguageToggle={withLanguageToggle}
       />
-      <section>
+      <section className={styles.title}>
         <Title title={titleJob} headingType="h1" />
       </section>
       <p>{descriptionJob}</p>
