97e85a3932ff12e34a233f8296d321802d10980f
jessiypao <93223636+jessiypao@users.noreply.github.com>
2023-05-15 09:05:21 -0500

    task/wa-32: created sendMessageChatbot analytics event (#57)

--- app/services/analytics/events/google-tag-events.service.ts
+++ app/services/analytics/events/google-tag-events.service.ts
@@ -9,6 +9,9 @@ const gtmEvents = {
   openChatbot(data: Record<string, any>, trigger = "open_chatbot", event = analyticsEvent): void {
     googleTagService.feedDataLayer(event, trigger, data)
   },
+  sendMessageChatbot(data: Record<string, any>, trigger = "send_message_chatbot", event = analyticsEvent): void {
+    googleTagService.feedDataLayer(event, trigger, data)
+  },
   sendMessageTextAnalysis(data: Record<string, any>, trigger = "send_message_text_analysis", event = analyticsEvent): void {
     googleTagService.feedDataLayer(event, trigger, data)
   },
