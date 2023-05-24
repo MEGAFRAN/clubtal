679272881ee2124c547727ce0f56d5c7a10f11e4
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2023-05-24 17:29:23 -0500

    Create logic to getPaths and content of files mdx

--- lib/getStaticAllPost.ts
+++ lib/getStaticAllPost.ts
@@ -1,15 +1,13 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import i18nextConfig from "../next-i18next.config"
+import { getMetaDataOfPostByLocale, sortPostByDate } from "./posts"
 import { Ctx, GetI18Props, MakeStaticProps } from "../app/constants/types/components_props/types"
-import { getAllPathsOfPost, getFileByLocaleAndTitle } from "./posts"
+import { getI18nPaths } from "./getStatic"
 
-export const getStaticPaths = () => {
-  const paths = getAllPathsOfPost()
-  return {
-    paths,
-    fallback: false,
-  }
-}
+export const getStaticPaths = () => ({
+  fallback: false,
+  paths: getI18nPaths(),
+})
 
 export async function getI18nProps({ ctx, ns = ["common"] }: GetI18Props) {
   const locale = ctx?.params?.locale || i18nextConfig.i18n.defaultLocale
@@ -21,12 +19,12 @@ export async function getI18nProps({ ctx, ns = ["common"] }: GetI18Props) {
 
 export function makeStaticProps({ ns = [] }: MakeStaticProps) {
   return async function getStaticProps(ctx: Ctx) {
-    const locale = ctx?.params?.locale || i18nextConfig.i18n.defaultLocale
-    const title = ctx?.params?.title as string
-    const { frontMatter, source } = await getFileByLocaleAndTitle({ locale, title })
+    const locale = ctx?.params?.locale || (i18nextConfig.i18n.defaultLocale as string)
+    const allPostData = await getMetaDataOfPostByLocale(locale)
+    const sortAllPostData = sortPostByDate(allPostData)
     const i18Configuration = await getI18nProps({ ctx, ns })
     return {
-      props: { frontMatter, source, ...i18Configuration },
+      props: { listPost: sortAllPostData, ...i18Configuration },
     }
   }
 }
