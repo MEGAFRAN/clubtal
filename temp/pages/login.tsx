c5a604f7a8963d81e837309a9d8f6748e5c435d1
Francisco cruz <megafran@msn.com>
2023-06-03 18:52:21 -0500

    feature/wa-63: resolved build errors

--- temp/pages/login.tsx
+++ temp/pages/login.tsx
@@ -1,28 +1,6 @@
 import { NextPage } from "next"
-import { useState } from "react"
 import LoginForm from "../../app/components/login_form/login_form"
-import createMagicLink from "../../app/services/form_services/create_magic_link/create-magic-link.service"
 
-const LoginPage: NextPage = () => {
-  const [isSubmitting, setIsSubmitting] = useState(false)
-  const [message, setMessage] = useState("")
-
-  const handleSubmit = async (email: string) => {
-    setIsSubmitting(true)
-    setMessage("Submitting data...")
-
-    const response = await createMagicLink(email)
-
-    if (response.ok) {
-      setMessage("Magic link sent to email, please verify.")
-    } else {
-      setMessage("Error: Please try filling the form again.")
-    }
-
-    setIsSubmitting(false)
-  }
-
-  return <LoginForm onSubmit={handleSubmit} isSubmitting={isSubmitting} message={message} />
-}
+const LoginPage: NextPage = () => <LoginForm />
 
 export default LoginPage
