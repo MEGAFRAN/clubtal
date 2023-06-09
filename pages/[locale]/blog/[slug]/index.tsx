dca6801710dc551465a777f3426b97bc6ef6038c
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2023-06-09 11:51:47 -0500

    Feauture: Add new styles to PostDetail

--- pages/[locale]/blog/[slug]/index.tsx
+++ pages/[locale]/blog/[slug]/index.tsx
@@ -1,6 +1,7 @@
 import { InferGetStaticPropsType } from "next"
 import { getStaticPathsEs, makeStaticProps } from "../../../../lib/getStaticPostDetail"
 import PostBody from "../../../../app/components/sections/content_post/PostBody"
+import styles from "../../../../app/styles/layouts/postDetail.module.scss"
 
 // ns ---> is variable of next-i18next that should to have array strings these strings are names of files that we want to translate in this page
 const ns = ["common"]
@@ -10,8 +11,10 @@ const getStaticPaths = getStaticPathsEs
 export { getStaticPaths, getStaticProps }
 
 const DetailPost = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => (
-  <div>
-    <h1>{post.title}</h1>
+  <div className={styles.container}>
+    <header>
+      <h1>{post.title}</h1>
+    </header>
     <PostBody content={post.content} />
   </div>
 )
