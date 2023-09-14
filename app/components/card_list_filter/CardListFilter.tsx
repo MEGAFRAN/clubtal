16234b5424c388402b3859e018aa55f58ea17f91
Francisco cruz <megafran@msn.com>
2023-09-14 18:47:33 -0500

    feature: solved modal outside click issue

--- app/components/card_list_filter/CardListFilter.tsx
+++ app/components/card_list_filter/CardListFilter.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react"
+import React, { FC, useState, useRef, useEffect } from "react"
 import styles from "../../styles/components/card-list-filter.module.scss"
 
 type CardListFilterProps = {
@@ -8,6 +8,8 @@ type CardListFilterProps = {
 
 const CardListFilter: FC<CardListFilterProps> = ({ specialities, onFilterChange }) => {
   const [selectedSpecialities, setSelectedSpecialities] = useState<string[]>([])
+  const dialogRef = useRef<HTMLDialogElement>(null)
+  const filterButtonRef = useRef<HTMLButtonElement>(null)
 
   const handleCheckboxChange = (speciality: string) => {
     if (selectedSpecialities.includes(speciality)) {
@@ -17,22 +19,76 @@ const CardListFilter: FC<CardListFilterProps> = ({ specialities, onFilterChange 
     }
   }
 
-  React.useEffect(() => {
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (dialogRef.current) {
+      const rect = dialogRef.current.getBoundingClientRect()
+      const isOutside =
+        event.clientX < rect.left ||
+        event.clientX > rect.right ||
+        event.clientY < rect.top ||
+        event.clientY > rect.bottom
+
+      if (isOutside) {
+        dialogRef.current.close()
+      }
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick)
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick)
+    }
+  }, [])
+
+  useEffect(() => {
     onFilterChange(selectedSpecialities)
   }, [selectedSpecialities, onFilterChange])
 
   return (
     <div className={styles.container}>
-      {specialities.map((speciality, index) => (
-        <label key={index} className={styles.label}>
-          <input
-            type="checkbox"
-            checked={selectedSpecialities.includes(speciality)}
-            onChange={() => handleCheckboxChange(speciality)}
-          />
-          {speciality}
-        </label>
-      ))}
+      <button
+        ref={filterButtonRef}
+        onClick={() => dialogRef.current?.showModal()}
+        className={styles.filter_button}
+      >
+        Filter
+      </button>
+      <div className={styles.scrollable_filters}>
+        {specialities.map(
+          (speciality, index) =>
+            speciality && (
+              <label key={index} className={styles.label}>
+                <input
+                  type="checkbox"
+                  checked={selectedSpecialities.includes(speciality)}
+                  onChange={() => handleCheckboxChange(speciality)}
+                />
+                {speciality}
+              </label>
+            ),
+        )}
+      </div>
+
+      <dialog ref={dialogRef} className={styles.dialog}>
+        {specialities.map(
+          (speciality, index) =>
+            speciality && (
+              <label key={index} className={styles.label}>
+                <input
+                  type="checkbox"
+                  checked={selectedSpecialities.includes(speciality)}
+                  onChange={() => handleCheckboxChange(speciality)}
+                />
+                {speciality}
+              </label>
+            ),
+        )}
+        <button onClick={() => dialogRef.current?.close()} className={styles.close_button}>
+          Close
+        </button>
+      </dialog>
     </div>
   )
 }
