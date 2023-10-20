8adc0afb1cdccf7bacc29f704f113e37987a9704
Francisco cruz <megafran@msn.com>
2023-10-20 18:15:18 -0500

    fix: solved sanity client import error

--- lib/sanity/client.ts
+++ lib/sanity/client.ts
@@ -1,18 +1,6 @@
 import { createClient } from "next-sanity"
 
-export const getClient = async (clientConfig: {
-  projectId: string
-  dataset: string
-  token: string
-  apiVersion: string
-}) =>
-  createClient({
-    ...clientConfig,
-    useCdn: false,
-    perspective: "published",
-  })
-
-export const client = createClient({
+const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
@@ -20,3 +8,5 @@ export const client = createClient({
   useCdn: false,
   perspective: "published",
 })
+
+export default client
