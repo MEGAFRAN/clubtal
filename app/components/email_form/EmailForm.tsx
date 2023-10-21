1c78400adbc5713d7acf06c7adeec4a60697ce30
Francisco cruz <megafran@msn.com>
2023-10-21 11:20:39 -0500

    feature: added for submit message

--- app/components/email_form/EmailForm.tsx
+++ app/components/email_form/EmailForm.tsx
@@ -5,28 +5,42 @@ import addCmsUserEmail from "../../services/cms/add-cms-user-email"
 
 const EmailForm = () => {
   const [email, setEmail] = useState("")
+  const [wasEmailSubmitted, setWasEmailSubmitted] = useState(false)
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
-    await downloadAsset(
-      "10_VERDADES_DE_LAS_VACANTES_DE_DESARROLLO_DE_SOFTWARE.pdf?sp=r&st=2023-10-18T23:09:28Z&se=2023-11-02T07:09:28Z&sv=2022-11-02&sr=b&sig=gWlsBfiVfw2B4tAx7qTaRp%2FuP1kmI4QMIK0ooKntfpQ%3D",
-      "10_VERDADES_DE_LAS_VACANTES_DE_DESARROLLO_DE_SOFTWARE.pdf",
-    )
-    await addCmsUserEmail(email)
+    try {
+      await downloadAsset(
+        "10_VERDADES_DE_LAS_VACANTES_DE_DESARROLLO_DE_SOFTWARE.pdf?sp=r&st=2023-10-18T23:09:28Z&se=2023-11-02T07:09:28Z&sv=2022-11-02&sr=b&sig=gWlsBfiVfw2B4tAx7qTaRp%2FuP1kmI4QMIK0ooKntfpQ%3D",
+        "10_VERDADES_DE_LAS_VACANTES_DE_DESARROLLO_DE_SOFTWARE.pdf",
+      )
+      if (!wasEmailSubmitted) setWasEmailSubmitted(true)
+      await addCmsUserEmail(email)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
-    <form className={styles.container} onSubmit={handleSubmit}>
-      <input
-        type="email"
-        placeholder="Escribe tu email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-        aria-label="Email address"
-        required
-      />
-      <button type="submit">Recibir Guía</button>
-    </form>
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Escribe tu email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          aria-label="Email address"
+          required
+        />
+        <button type="submit">Recibir Guía</button>
+      </form>
+      {wasEmailSubmitted && (
+        <p>
+          Gracias, pronto recibirás información valiosa, para llevar al siguiente nivel tu carrera
+          en desarrollo de software
+        </p>
+      )}
+    </div>
   )
 }
 
