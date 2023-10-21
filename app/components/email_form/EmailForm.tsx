9c3da00ac64738fd4a8e5eff52d2113585931e13
Francisco cruz <megafran@msn.com>
2023-10-21 10:56:07 -0500

    feature: metatag updates

--- app/components/email_form/EmailForm.tsx
+++ app/components/email_form/EmailForm.tsx
@@ -8,11 +8,11 @@ const EmailForm = () => {
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
-    await addCmsUserEmail(email)
     await downloadAsset(
       "10_VERDADES_DE_LAS_VACANTES_DE_DESARROLLO_DE_SOFTWARE.pdf?sp=r&st=2023-10-18T23:09:28Z&se=2023-11-02T07:09:28Z&sv=2022-11-02&sr=b&sig=gWlsBfiVfw2B4tAx7qTaRp%2FuP1kmI4QMIK0ooKntfpQ%3D",
       "10_VERDADES_DE_LAS_VACANTES_DE_DESARROLLO_DE_SOFTWARE.pdf",
     )
+    await addCmsUserEmail(email)
   }
 
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,9 @@
 import React from "react"
-import { getStaticPropsIndexContent } from "../lib/pages"
 import PageHead from "../app/components/page_head/PageHead"
 import { IndexContent } from "../app/constants/types/content_models/types"
-import HeaderSearch from "../app/components/sections/above_fold/header_search/HeaderSearch"
-import { Company } from "../app/constants/interfaces/content_models/interfaces"
-import cmsCrud from "../lib/sanity/crud/crud"
-import BottomBar from "../app/components/bottomBar/BottomBar"
 import styles from "../app/styles/pages/index.module.scss"
-import Breadcrumb from "../app/components/breadcrumb/Breadcrumb"
 import EmailForm from "../app/components/email_form/EmailForm"
-import MagicLinkLogin from "../app/components/magic_link_login/MagicLinkLogin"
 import SectionUnderlineList from "../app/components/sections/underline_list/SectionUnderlineList"
-import downloadAsset from "../app/services/form_services/download_asset/download-asset.service"
 import SectionAnalytics from "../app/components/sections/analytics/SectionAnalytics"
 
 // const getStaticProps = getStaticPropsIndexContent
@@ -19,17 +11,25 @@ import SectionAnalytics from "../app/components/sections/analytics/SectionAnalyt
 const Home = ({ homePageData, categories }: IndexContent) => (
   // const { metaDescription, headerTitle } = homePageData || {}
   <>
-    <PageHead description={""} title={"clubtal"} locale={"es"} url={"https://www.clubtal.dev"} />
+    <PageHead
+      description={
+        "Mejora tu salario y calidad de vida como desarrollador de software. Aprende habilidades blandas como comunicación, resolución de problemas, negociación y mas"
+      }
+      title={
+        "Mejora Tu Salario y Calidad de Vida como Desarrollador de Software con habilidades Blandas | Clubtal"
+      }
+      locale={"es"}
+      url={"https://clubtal.dev"}
+    />
     <section className={styles.container}>
       <header>
         <h1>
           Mejoramos tu salario y calidad de vida como desarrollador de software, enseñándote
-          habilidades blandas
+          habilidades blandas: (comunicación, resolución de problemas, negociación y mas)
         </h1>
-        <p>*Pre-Lanzamiento</p>
         <p>
           <strong>
-            Registrate y recibe gratis: Las 10 verdades de las vacantes de desarrollo de software
+            Regístrate y recibe gratis: Las 10 verdades de las vacantes de desarrollo de software
           </strong>
         </p>
         <p>#1: Juego de probabilidades, #2: No siempre gana el mejor y 8 verdades más</p>
@@ -41,42 +41,42 @@ const Home = ({ homePageData, categories }: IndexContent) => (
           "Cursos",
           "Videos",
           "Ebooks",
-          "Articulos",
+          "Artículos",
           "Contacto con especialistas",
           "Respuestas en vivo",
         ]}
-        title="¿Que beneficios tendras?"
+        title="¿Qué beneficios tendrás?"
         buttonText="Quiero unirme"
         sectionToScroll="section-underline-list"
       />
       <section className={styles.why_join}>
         <h2>¿Por qué unirse?</h2>
         <p>
-          CLUBTAL fue creado por un desarrollador, que durante la pandemia de COVID19, dejó su
+          CLUBTAL fue creado por un desarrollador que, durante la pandemia de COVID-19, dejó su
           profesión como médico para aprender programación y así evitar que su familia se
           contagiara.
         </p>
         <p>
-          Durante meses su familia vivió de ahorros, mientras aplicaba a vacantes Junior de
-          desarrollo. Consiguió su primer trabajo como desarrollador Frontend Angular, sin siquiera
-          saber git, angular o typescript !!!
+          Durante meses su familia vivió de ahorros mientras aplicaba a vacantes Junior de
+          desarrollo. Consiguió su primer trabajo como desarrollador Frontend Angular, ¡sin siquiera
+          saber git, angular o typescript!
         </p>
         <p>
           <strong>
-            Todo gracias a las habilidades blandas y estrategias usadas en el proceso de selección
+            Todo gracias a las habilidades blandas y estrategias usadas en el proceso de selección.
           </strong>
         </p>
         <p>
-          Actualmente trabaja para una multinacional de USA, y gana más de 11 veces el salario de su
-          primer empleo
+          Actualmente trabaja para una multinacional de USA y gana más de 11 veces el salario de su
+          primer empleo.
         </p>
-        <p>Tu puedes lograr esto y mucho más</p>
+        <p>Tú puedes lograr esto y mucho más.</p>
         <p>
           <strong>¿Quieres mejorar tu calidad de vida y salario como desarrollador?</strong>
         </p>
-        <p>Únete a la lista de espera, y recibe gratis en tu correo:</p>
+        <p>Únete a la lista de espera y recibe gratis en tu correo:</p>
         <p>
-          <strong>Las 10 verdades de las vacantes de desarrollo de software</strong>
+          <strong>Las 10 verdades de las vacantes de desarrollo de software.</strong>
         </p>
         <EmailForm />
       </section>
